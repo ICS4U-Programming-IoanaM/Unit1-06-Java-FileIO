8fd83022840e5850de7a390a11e762eb987dde8e
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-04-02 13:05:24 +0000

    fixed linter issues and added comments to calcSum().

--- FileIO.java
+++ FileIO.java
@@ -3,27 +3,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * This program reads and writes to a file the sum of a set of ints.
+ *
+ * @author Ioana Marinescu
+ * @version 1.0
+ * @since 2024-04-01
+ */
+
+// sourced from https://chat.openai.com
+@SuppressWarnings("HideUtilityClassConstructor")
 public final class FileIO {
-  // constructor
+  // Constructor for the FileIO class.
   public FileIO() {
     // empty constructor
   }
 
-  public static String calcSum(final String[] fileArrayStr, int sum) {
-    try {
-      int[] fileArray = new int[fileArrayStr.length];
+  /**
+   * Calculates the sum of integers from a string array.
+   *
+   * @param fileArrayStr Array of strings representing integers.
+   * @return String representation of the sum.
+   */
+  public static String calcSum(final String[] fileArrayStr) {
+    // variables
+    int sum = 0;
+    int[] fileArray = new int[fileArrayStr.length];
 
+    try {
+      // loops until index = length of array
       for (int index = 0; index < fileArrayStr.length; index++) {
+        // converts each element in array to int and adds it to int array
         fileArray[index] = Integer.parseInt(fileArrayStr[index]);
+
+        // calculates the sum of int being added to array
         sum += fileArray[index];
       }
+      // return sum as string
       return String.valueOf(sum);
 
+      // fails to convert to int
     } catch (NumberFormatException e) {
       return "Invalid input, could not calculate.";
     }
   }
 
+  /**
+   * Main method to read input from a file, calculate sum, and write to an output
+   * file.
+   *
+   * @param args Command line arguments (unused).
+   */
   public static void main(final String[] args) {
     try {
       // finding input file and setting scanner to read it
@@ -39,11 +69,8 @@ public final class FileIO {
         // reads line in file and splits it by space char
         String[] fileArrayStr = scanner.nextLine().split(" ");
 
-        // variables
-        int sum = 0;
-
         // calling function and writing to file
-        writer.write(calcSum(fileArrayStr, sum));
+        writer.write(calcSum(fileArrayStr));
         writer.write("\n");
       }
       // message to console confirming the process finished with no errors
